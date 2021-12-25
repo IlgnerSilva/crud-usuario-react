94418b895bc6839495edd67d0af86cc7d92df725
IlgnerSilva <ilgnersilva@outlook.com>
2021-12-25 12:50:35 -0300

    Fazendo GET de cartões

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -1,18 +1,35 @@
-import React, { Component } from "react"
-import Main from "../templates/Main"
+import React, { Component } from "react";
+import Main from "../templates/Main";
+import axios from "axios";
 
 const headerProps = {
-    icon: 'users',
-    title: 'Usuários',
-    subtitle: 'Cadastro de usuários'
-}
+  icon: "users",
+  title: "Usuários",
+  subtitle: "Cadastro de usuários",
+};
 
-export default class UserCrud extends Component{
-    render(){
-        return(
-            <Main {...headerProps}>
-                cadastro de usuário
-            </Main>
-        )
-    }
+export default class UserCrud extends Component {
+  state = {
+    persons: [],
+  };
+
+  componentDidMount() {
+    axios.get(`https://api-pagamento-m4.herokuapp.com/pagamento`).then((res) => {
+      const persons = res.data.Users;
+      console.log(persons)
+      this.setState({ persons });
+    });
+  }
+
+  render() {
+    return (
+      <Main {...headerProps}>
+        <ul>
+          {this.state.persons.map((person) => (
+            <li>{`ID:${person.ID} NUMERO CARTÃO: ${person.NUMERO} TITULAR: ${person.NOME_DO_TITULAR} VALIDADE:${person.VALIDADE}`}</li>
+          ))}
+        </ul>
+      </Main>
+    );
+  }
 }
